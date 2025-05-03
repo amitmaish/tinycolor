038cc93817ed32d3b23922a2140d3f93ab77cb43
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-05-03 12:16:18 -0700

    added color constants

--- src/color.rs
+++ src/color.rs
@@ -9,9 +9,51 @@ pub struct srgb {
 }
 
 impl srgb {
+    pub const WHITE: srgb = srgb {
+        r: 1.0,
+        g: 1.0,
+        b: 1.0,
+    };
+
+    pub const BLACK: srgb = srgb {
+        r: 0.0,
+        g: 0.0,
+        b: 0.0,
+    };
+
     pub const RED: srgb = srgb {
         r: 1.0,
         g: 0.0,
         b: 0.0,
+    };
+
+    pub const YELLOW: srgb = srgb {
+        r: 1.0,
+        g: 1.0,
+        b: 0.0,
+    };
+
+    pub const GREEN: srgb = srgb {
+        r: 0.0,
+        g: 1.0,
+        b: 0.0,
+    };
+
+    pub const AQUA: srgb = srgb {
+        r: 0.0,
+        g: 1.0,
+        b: 1.0,
+    };
+
+    pub const BLUE: srgb = srgb {
+        r: 0.0,
+        g: 0.0,
+        b: 1.0,
+    };
+
+    pub const PURPLE: srgb = srgb {
+        r: 1.0,
+        g: 0.0,
+        b: 1.0,
     };
 }
