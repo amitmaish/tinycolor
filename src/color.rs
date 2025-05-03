8cef91ed4494f61ae9fed0927b44cc0e49d1abb8
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-05-03 11:20:43 -0700

    updated colors

--- src/color.rs
+++ src/color.rs
@@ -1,9 +1,17 @@
 #![allow(dead_code, non_camel_case_types)]
 
 #[repr(C)]
-#[derive(Clone, Copy, Default)]
+#[derive(Clone, Copy, Debug, Default)]
 pub struct srgb {
     pub r: f32,
     pub g: f32,
     pub b: f32,
+}
+
+impl srgb {
+    pub const RED: srgb = srgb {
+        r: 1.0,
+        g: 0.0,
+        b: 0.0,
+    };
 }
