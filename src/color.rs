a52932f622ad9d4d3c649058be977a7c8ff4fa58
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-05-11 19:29:34 -0700

    added linear rgb, as well as automatic converting between colors and arrays

--- src/color.rs
+++ src/color.rs
@@ -57,3 +57,79 @@ impl srgb {
         b: 1.0,
     };
 }
+
+impl From<[f32; 3]> for srgb {
+    fn from(value: [f32; 3]) -> Self {
+        Self {
+            r: value[0],
+            g: value[1],
+            b: value[2],
+        }
+    }
+}
+
+impl From<srgb> for [f32; 3] {
+    fn from(value: srgb) -> Self {
+        [value.r, value.g, value.b]
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default)]
+pub struct rgb {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+impl From<[f32; 3]> for rgb {
+    fn from(value: [f32; 3]) -> Self {
+        Self {
+            r: value[0],
+            g: value[1],
+            b: value[2],
+        }
+    }
+}
+
+impl From<rgb> for [f32; 3] {
+    fn from(value: rgb) -> Self {
+        [value.r, value.g, value.b]
+    }
+}
+
+impl From<rgb> for srgb {
+    fn from(value: rgb) -> Self {
+        Self {
+            r: to_linear(value.r),
+            g: to_linear(value.g),
+            b: to_linear(value.b),
+        }
+    }
+}
+
+impl From<srgb> for rgb {
+    fn from(value: srgb) -> Self {
+        Self {
+            r: from_linear(value.r),
+            g: from_linear(value.g),
+            b: from_linear(value.b),
+        }
+    }
+}
+
+fn from_linear(x: f32) -> f32 {
+    if x >= 0.0031308 {
+        (1.055) * x.powf(1.0 / 2.4) - 0.055
+    } else {
+        12.92 * x
+    }
+}
+
+fn to_linear(x: f32) -> f32 {
+    if x >= 0.04045 {
+        ((x + 0.055) / (1.0 + 0.055)).powf(2.4)
+    } else {
+        x / 12.92
+    }
+}
